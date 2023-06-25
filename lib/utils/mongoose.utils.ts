e556266aed299cbfddadba7d47facd6ca1c0f7ee
mlazuardy <lazuardymichael@gmail.com>
2023-06-25 16:11:23 +0700

    chore: change retryWhen

--- lib/utils/mongoose.utils.ts
+++ lib/utils/mongoose.utils.ts
@@ -1,6 +1,6 @@
 import { Logger } from "@nestjs/common";
-import { Observable } from "rxjs";
-import { delay, retryWhen, scan } from "rxjs/operators";
+import { Observable, timer } from "rxjs";
+import { retry } from "rxjs/operators";
 import {
   MODEL_KEY,
   TYPEGOOSE_DEFAULT_CONNECTION,
@@ -24,25 +24,25 @@ export function handleRetry(
   retryAttempts = 9,
   retryDelay = 3000,
 ): <T>(source: Observable<T>) => Observable<T> {
-  const logger = new Logger("MongooseModule");
-  return <T>(source: Observable<T>) =>
-    source.pipe(
-      retryWhen((e) =>
-        e.pipe(
-          scan((errorCount, error) => {
-            logger.error(
-              `Unable to connect to the database. Retrying (${
-                errorCount + 1
-              })...`,
-              "",
-            );
-            if (errorCount + 1 >= retryAttempts) {
-              throw error;
-            }
-            return errorCount + 1;
-          }, 0),
-          delay(retryDelay),
-        ),
-      ),
+  const logger = new Logger("TypegooseModule");
+
+  return <T>(source: Observable<T>) => {
+    return source.pipe(
+      retry({
+        count: retryAttempts,
+        delay: (error, errorCount) => {
+          if (errorCount >= retryAttempts) {
+            logger.error(`Exceeded maximum retry attempts.`);
+            throw error;
+          }
+          errorCount++;
+          logger.error(
+            `Unable to connect to the database. Retrying (${errorCount})...`,
+            "",
+          );
+          return timer(retryDelay);
+        },
+      }),
     );
+  };
 }
